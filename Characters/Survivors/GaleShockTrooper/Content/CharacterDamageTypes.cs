2fbe1403f2b51e626b959645b60d3e86f6c51ede
Moffein <22742925+Moffein@users.noreply.github.com>
2025-07-19 13:43:55 -0500

    Add project files.

---
+++ Characters/Survivors/GaleShockTrooper/Content/CharacterDamageTypes.cs
@@ -0,0 +1,14 @@
+﻿using R2API;
+
+namespace GaleShockTrooper.Survivors.GaleShockTrooper.HenryContent
+{
+    public class CharacterDamageTypes
+    {
+        public static DamageAPI.ModdedDamageType comboFinisherDebuffDamage;
+
+        public static void Init()
+        {
+            comboFinisherDebuffDamage = DamageAPI.ReserveDamageType();
+        }
+    }
+}
---
+++ Characters/Survivors/GaleShockTrooper/Content/CharacterUnlockables.cs
@@ -0,0 +1,20 @@
+﻿using GaleShockTrooper.Survivors.GaleShockTrooper.Achievements;
+using RoR2;
+using UnityEngine;
+
+namespace GaleShockTrooper.Survivors.GaleShockTrooper.HenryContent
+{
+    public static class CharacterUnlockables
+    {
+        public static UnlockableDef characterUnlockableDef = null;
+        public static UnlockableDef masterySkinUnlockableDef = null;
+
+        public static void Init()
+        {
+            masterySkinUnlockableDef = Modules.Content.CreateAndAddUnlockbleDef(
+                MasteryAchievement.unlockableIdentifier,
+                Modules.Tokens.GetAchievementNameToken(MasteryAchievement.identifier),
+                GaleShockTrooperSurvivor.instance.assetBundle.LoadAsset<Sprite>("texMasteryAchievement"));
+        }
+    }
+}
